083b6a0da824b15b9cc22714a3f34d5f3034c5e0
Madson <dev@MacBook-Pro-de-Madson.local>
2011-09-29 19:43:16 -0300

    Initial Commit

---
+++ NavigationControllerExample/NavigationControllerExampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NavigationControllerExampleAppDelegate.h
+//  NavigationControllerExample
+//
+//  Created by Madson on 29/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationControllerExampleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
